61530a76bf659c6d4fd6b6a4a9bb09c997fbb78f
Anjali MahawR <anjalimahawar454@gmail.com>
2022-01-30 09:17:33 -0800

    first temp

--- src/App.js
+++ src/App.js
@@ -1,25 +1,36 @@
-import logo from './logo.svg';
+//import logo from './logo.svg';
 import './App.css';
+import { BrowserRouter as Router , Routes , Route} from 'react-router-dom';
+import Error404 from './page/Error404';
+import Home from './page/Home';
+import Login from './page/Login';
+import Register from './page/Register';
 
+
+//rfc
 function App() {
+//state
+
+
+//function
+
+//return
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    
+         <Router>
+            <Routes>
+                 <Route path="/" element={<Home/>}/>
+             //     <Route Path="/Login" element={<Login/>}/>
+                  <Route path="/Login" element={<Login/>}/>
+                 <Route path="/Register" element={<Register/>}/>
+                 <Route path="*" element={<Error404/>}/>
+
+                
+            </Routes>
+         </Router>
+   );
 }
+
 
 export default App;
